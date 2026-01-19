479a3fae56a9ba59bda98fec6d53146f314359d5
Billuc <billuc@hotmail.fr>
2026-01-19 23:17:31 +0100

    Initiated database

--- astro/db/config.ts
+++ astro/db/config.ts
@@ -1,6 +1,16 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from "astro:db";
+
+const File = defineTable({
+  columns: {
+    id: column.text(), // Random key format '[a-z]+(-[a-z]+){3}'
+    name: column.text(),
+    content: column.text(),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    File,
+  },
 });
--- astro/db/seed.ts
+++ astro/db/seed.ts
@@ -1,6 +1,25 @@
-import { db } from 'astro:db';
+import { db, File } from "astro:db";
+import { generateRandomKey } from "../src/utils/generateKey";
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  const sampleFiles = [
+    {
+      id: "aaaaa-bbbbb-ccccc-ddddd",
+      name: "File 1",
+      content: "This is the content of File 1.",
+    },
+    {
+      id: "bbbbb-ccccc-ddddd-eeeee",
+      name: "File 2",
+      content: "This is the content of File 2.",
+    },
+    {
+      id: "ccccc-ddddd-eeeee-fffff",
+      name: "File 3",
+      content: "This is the content of File 3.",
+    },
+  ];
+
+  await db.insert(File).values(sampleFiles);
 }
