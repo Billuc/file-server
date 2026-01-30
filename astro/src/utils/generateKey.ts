738268dbbb6cae0f1d0c7205e3eb004930f04bbf
Billuc <billuc@hotmail.fr>
2026-01-30 21:52:07 +0100

    Updated key generation function to use nouns from csv

--- astro/src/utils/generateKey.ts
+++ astro/src/utils/generateKey.ts
@@ -1,22 +1,22 @@
 // Utility function to generate random keys in the format: [a-z]+(-[a-z]+){3}
 // Example: aaaaa-bbbbb-ccccc-ddddd
+import { webcrypto as crypto } from "node:crypto";
+import { readFile } from "node:fs/promises";
 
-export function generateRandomKey(): string {
-  const generateSegment = (): string => {
-    const chars = 'abcdefghijklmnopqrstuvwxyz';
-    let segment = '';
-    for (let i = 0; i < 5; i++) {
-      segment += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return segment;
-  };
+const MAX_VALUE = 512;
 
-  const segments = [
-    generateSegment(),
-    generateSegment(),
-    generateSegment(),
-    generateSegment(),
-  ];
+export async function generateRandomKey(): Promise<string> {
+  const requiredBits = Math.ceil(Math.log2(MAX_VALUE));
+  const nounsFile = await readFile("src/assets/keys/keys.csv", "utf-8");
+  const nouns = nounsFile.split("\n").map((line) => line.split(","));
 
-  return segments.join('-');
+  const values = crypto.getRandomValues(new Uint16Array(4));
+  const segments: string[] = [];
+
+  for (const value of values) {
+    const index = value >> (16 - requiredBits);
+    segments.push(nouns[index][0]);
+  }
+
+  return segments.join("-");
 }
