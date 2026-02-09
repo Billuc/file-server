20a615da86f2950c442cb9e828919cbf076ddaaa
Billuc <billuc@hotmail.fr>
2026-02-09 22:09:08 +0100

    Added logs

--- astro/main.mjs
+++ astro/main.mjs
@@ -8,9 +8,22 @@ launchCleanup();
 console.log("Cleanup service launched !");
 
 const base = process.env.ASTRO_BASE || "/";
-console.log("using base", base);
+console.log("Using base", base);
 
 const app = express();
+
+app.use(function (req, res, next) {
+    const uuid = crypto.randomUUID();
+    console.log(uuid, "- RECV", req.method, req.url);
+    const timer = console.time(uuid);
+    
+    res.on("finish", () => {
+        console.log(uuid, "- SENT", res.statusCode, req.url);
+        console.timeEnd(uuid);
+    });
+    next();
+});
+
 app.use(base, express.static("dist/client/"));
 app.use(ssrHandler);
 
