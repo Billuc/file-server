a01d5531040ff2df05c8758d55dfb55f793748bc
Billuc <billuc@hotmail.fr>
2026-03-09 11:57:21 +0100

    fix: serving static files correctly

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 import db from "@astrojs/db";
 import deno from "@deno/astro-adapter";
-import { loadEnvFile, env } from "node:process";
+import { loadEnvFile, env } from "node:process"; // Using Node env for Astro
 
 loadEnvFile();
 console.log("base is", env.ASTRO_BASE);
--- astro/main.mjs
+++ astro/main.mjs
@@ -1,18 +1,26 @@
 import { handle as ssrHandle } from "./dist/server/entry.mjs";
 import { launchCleanup } from "./launchCleanup.mjs";
-import { loadEnvFile, env } from "node:process";
+import { loadEnvFile, env } from "node:process"; // Using Node env for Astro
 import { serveDir } from "jsr:@std/http";
 
 loadEnvFile();
 launchCleanup();
 console.log("Cleanup service launched !");
 
+const base = env.ASTRO_BASE || "/";
+const port = parseInt(env.ASTRO_PORT || "3000");
+console.log("Starting server on port", port, "with base", base);
+
 async function handler(req) {
   const uuid = crypto.randomUUID();
   console.log(uuid, "- RECV", req.method, req.url);
   const start = performance.now();
 
-  let res = await serveDir(req, { fsRoot: "./dist/client", quiet: true });
+  let res = await serveDir(req, {
+    fsRoot: "./dist/client",
+    quiet: true,
+    urlRoot: base.slice(1), // Remove leading slash
+  });
   if (!res.ok) {
     res = await ssrHandle(req);
   }
@@ -31,8 +39,5 @@ async function handler(req) {
   return res;
 }
 
-const base = env.ASTRO_BASE || "/";
-const port = parseInt(env.ASTRO_PORT || "3000");
-console.log("Starting server on port", port, "with base", base);
 Deno.serve({ port, hostname: "0.0.0.0" }, handler);
 console.log("Server started on port", port);
