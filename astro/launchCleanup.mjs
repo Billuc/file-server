25bb95ca9918ed025a0451d7ab240691d416d027
Billuc <billuc@hotmail.fr>
2026-03-18 08:56:12 +0100

    feat: using Bun.sql instead of libsql/client

--- astro/launchCleanup.mjs
+++ astro/launchCleanup.mjs
@@ -1,34 +1,36 @@
-import { env } from "node:process";
 import { rm, readdir } from "node:fs/promises";
-import { createClient } from "@libsql/client";
-
-const SELECT_QUERY =
-  "SELECT id, name FROM files WHERE expiresAt >= datetime('now');";
-const DELETE_QUERY = "DELETE FROM files WHERE expiresAt < datetime('now');";
 
 const UPLOAD_PATH = "./uploads/";
 
 export function launchCleanup() {
-  const dbPath = env["ASTRO_DB_REMOTE_URL"];
+  const dbPath = Bun.env.ASTRO_DB_REMOTE_URL;
   if (!dbPath) {
     throw new Error(
       "ASTRO_DB_REMOTE_URL is not defined in environment variables.",
     );
   }
 
-  const client = createClient({ url: dbPath });
+  const client = new Bun.SQL({
+    adapter: "sqlite",
+    filename: dbPath,
+  });
+  console.log("Launching cleanup...");
   cleanup(client);
   setInterval(() => cleanup(client), 60 * 60 * 1000); // Run every hour
 }
 
-async function cleanup(client) {
-  const notExpiredFiles = await client.execute(SELECT_QUERY);
+/**
+ * @param {Bun.SQL} sql
+ */
+async function cleanup(sql) {
+  const notExpiredFiles =
+    await sql`SELECT id, name FROM files WHERE expiresAt >= datetime('now');`.values();
 
   try {
     const files = await readdir(UPLOAD_PATH);
 
     for (const file of files) {
-      const isNotExpired = notExpiredFiles.rows.find((row) => {
+      const isNotExpired = notExpiredFiles.find((row) => {
         const id = row[0];
         const name = row[1];
         return file === `${id}__${name}`;
@@ -43,5 +45,5 @@ async function cleanup(client) {
     console.error("Could not read upload directory", err);
   }
 
-  await client.execute(DELETE_QUERY);
+  await sql`DELETE FROM files WHERE expiresAt < datetime('now');`;
 }
